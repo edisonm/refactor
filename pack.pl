d790efe153a8ec7dc8a28746f0d9ddd44261b335
Edison Mera <mera@process-design-center.com>
2013-03-22 21:53:58 +0100

    Fixed version in pack

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(refactor).
-version('0.1.0').
+version('0.0.2').
 title('Refactoring Tools for SWI-Prolog').
 keywords([refactor]).
 author('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
