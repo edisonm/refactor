1f86a9eba4416d38444d609b012f46f3e2f0d053
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2013-04-07 21:53:33 +0200

    Allow not to bind the pattern to the term (not really used)

--- prolog/term_info.pl
+++ prolog/term_info.pl
@@ -27,7 +27,10 @@
     the GNU General Public License.
 */
 
-:- module(term_info, [get_term_info/4]).
+:- module(term_info,
+	  [ get_term_info/4,
+	    get_term_info/5
+	  ]).
 :- use_module(library(prolog_source)).
 
 % BUG: Files are not uniques
@@ -50,28 +53,31 @@ module_file_2(M, File0, File) :-
     source_file_property(File0, includes(File1,_)),
     module_file_2(M, File1, File).
 
-get_term_info(M, Term, File, Options) :-
+get_term_info(M, Pattern, File, Options) :-
+	get_term_info(M, Pattern, Term, File, Options),
+	Term = Pattern.
+
+get_term_info(M, Pattern, Term, File, Options) :-
     module_files(M, Files),
     member(File, Files),
-    get_term_info_file(Term, File, [module(M)|Options]).
+    get_term_info_file(Pattern, Term, File, [module(M)|Options]).
 
-get_term_info_file(Term, File, Options) :-
+get_term_info_file(Pattern, Term, File, Options) :-
 	catch(setup_call_cleanup(
 		  ( prolog_canonical_source(File, Path),
 		    prolog_open_source(Path, In)
 		  ),
-		  get_term_info_fd(In, Term, Options),
+		  get_term_info_fd(In, Pattern, Term, Options),
 		  prolog_close_source(In)),
 	  E, (print_message(error, E), fail)).
 
-get_term_info_fd(In, Ref, Options) :-
+get_term_info_fd(In, Pattern, Term, Options) :-
     repeat,
     prolog_read_source_term(In, Term, _Expanded, Options),
     ( Term == end_of_file ->
       !,
       fail
-    ; subsumes_term(Ref, Term),
-      Ref = Term
+    ; subsumes_term(Pattern, Term)
     ).
 
 % match_reference(F/A,   Term) :- functor(Term, F, A).
