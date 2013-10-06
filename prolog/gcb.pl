b96c73aa7c0a553cdb64c5c60cd30ff2cee39d4b
Edison Mera <efmera@gmail.com>
2013-10-06 11:48:35 +0200

    Added comment about a bug

--- prolog/gcb.pl
+++ prolog/gcb.pl
@@ -1,10 +1,28 @@
 :- module(gcb, [greatest_common_binding/6, substitute_list/3]).
 
+% ?- greatest_common_binding(h(f(A)),g(A,f(A)),T,I,B,[]).
+% T = h(_G357),
+% I = g(A, _G357),
+% B = [_G357=f(A)].
+
+% intersect_eq([], _, []).
+% intersect_eq([Elem0|T], L, Set0) :-
+%     ( member(Elem, L),
+%       Elem0 == Elem ->
+%       Set0 = [Elem0|Set]
+%     ; Set = Set0
+%     ),
+%     intersect_eq(T, L, Set).
+
 greatest_common_binding(Term0, Into0, Term, Into) -->
     ( {var(Term0);var(Into0)} ->
       {Term=Term0, Into=Into0}
     ; {substitute(Var=Term0, Into0, Into),
-       Into0\==Into} ->
+       Into0\==Into
+       % term_variables(Term0, Var0),
+       % term_variables(Into, Var1),
+       % intersect_eq(Var0, Var1, [])
+      } ->
       [Var=Term0],
       {Term=Var}
     ; greatest_common_binding(1, Term0, Into0, Term, Into),
