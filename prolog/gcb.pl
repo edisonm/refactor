f43c2a1191154f0f5ce7f6bd1ade8024f70b3800
Edison Mera <mera@process-design-center.com>
2013-10-07 11:13:32 +0200

    - greatest_common_binding only perform replacements if that do not   generate unlinked variables in Into.
    - Now we don't calculate Bindings, they can be calculated using the
      predicate unifiable/3

--- prolog/gcb.pl
+++ prolog/gcb.pl
@@ -1,45 +1,52 @@
-:- module(gcb, [greatest_common_binding/6, substitute_list/3]).
+:- module(gcb, [greatest_common_binding/4,
+		greatest_common_binding/7,
+		substitute_list/3]).
 
 % ?- greatest_common_binding(h(f(A)),g(A,f(A)),T,I,B,[]).
 % T = h(_G357),
 % I = g(A, _G357),
 % B = [_G357=f(A)].
 
-% intersect_eq([], _, []).
-% intersect_eq([Elem0|T], L, Set0) :-
-%     ( member(Elem, L),
-%       Elem0 == Elem ->
-%       Set0 = [Elem0|Set]
-%     ; Set = Set0
-%     ),
-%     intersect_eq(T, L, Set).
+subtract_eq([], _, []).
+subtract_eq([Elem0|T], L, Set0) :-
+    ( member(Elem, L),
+      Elem0 == Elem ->
+      Set0 = Set
+    ; Set0 = [Elem0|Set]
+    ),
+    subtract_eq(T, L, Set).
 
-greatest_common_binding(Term0, Into0, Term, Into) -->
-    ( {var(Term0);var(Into0)} ->
-      {Term=Term0, Into=Into0}
-    ; {substitute(Var=Term0, Into0, Into),
-       Into0\==Into
-       % term_variables(Term0, Var0),
-       % term_variables(Into, Var1),
-       % intersect_eq(Var0, Var1, [])
-      } ->
-      [Var=Term0],
-      {Term=Var}
-    ; greatest_common_binding(1, Term0, Into0, Term, Into),
-      {Into0\==Into}
-    ->[]
-    ; {Term=Term0, Into=Into0}
+common_vars(Term, Into, Vars) :-
+    term_variables(Term, VTerm),
+    term_variables(Into, VInto),
+    subtract_eq(VInto, VTerm, Vars).
+
+greatest_common_binding(Term0, Into0, Term, Into) :-
+    common_vars(Term0, Into0, Vars0),
+    greatest_common_binding(Term0, Term0, Into0, Vars0, Term, Into, _).
+
+greatest_common_binding(SubTerm, Term0, Into0, Vars0, Term, Into, Vars) :-
+    ( (var(SubTerm);var(Into0)) ->
+      Term=Term0, Into=Into0
+    ; substitute(Var=SubTerm, Into0, Into),
+      Into0\==Into,
+      substitute(Var=SubTerm, Term0, Term),
+      common_vars(Term, Into, Vars),
+      subtract_eq(Vars, Vars0, [])
+    ->true % [Var=SubTerm]
+    ; greatest_common_binding(1, SubTerm, Term0, Into0, Vars0, Term, Into, Vars),
+      Into0\==Into
+    ->true
+    ; Term=Term0, Into=Into0
     ).
 
-greatest_common_binding(N, Term0, Into0, Term, Into) -->
-    {arg(N, Term0, Arg)},
+greatest_common_binding(N, SubTerm, Term0, Into0, Vars0, Term, Into, Vars) :-
+    arg(N, SubTerm, Arg),
     !,
-    pick_tail(Tail),
-    greatest_common_binding(Arg, Into0, _, Into1),
-    {substitute_olist(Tail, Term0, Term1)},
-    {succ(N, N1)},
-    greatest_common_binding(N1, Term1, Into1, Term, Into).
-greatest_common_binding(_, Term, Into, Term, Into) --> [].
+    greatest_common_binding(Arg, Term0, Into0, Vars0, Term1, Into1, Vars1),
+    succ(N, N1),
+    greatest_common_binding(N1, SubTerm, Term1, Into1, Vars1, Term, Into, Vars).
+greatest_common_binding(_, _, Term, Into, Vars, Term, Into, Vars).
 
 substitute_olist(Tail) --> {var(Tail)}, !.
 substitute_olist([Subst|Tail]) -->
