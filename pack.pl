10b1ebef4c7f9b0f75be7642eda37bf2a2aebebe
Edison Mera <mera@process-design-center.com>
2014-04-10 16:49:10 +0200

    Added pack dependencies

--- pack.pl
+++ pack.pl
@@ -7,3 +7,5 @@ packager('Edison Mera Menendez', 'http://www.edisonm.com/').
 maintainer('Edison Mera Menendez', 'http://www.edisonm.com/').
 home('https://github.com/edisonm/refactor').
 download('https://github.com/edisonm/refactor.git').
+requires(xtools).
+requires(xlibrary).
