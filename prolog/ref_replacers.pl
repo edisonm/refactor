e36860e58e2d0d97a305071d2723db1f00cdf0bb
Edison Mera <mera@process-design-center.com>
2016-08-03 17:24:43 +0200

    Added replacer replace/4, that is replace/5 when the Expander is true

--- prolog/ref_replacers.pl
+++ prolog/ref_replacers.pl
@@ -3,7 +3,8 @@
 	   replace_head/4,
 	   replace_body/4,
 	   replace_goal/4,
-	   replace_sentence/4
+	   replace_sentence/4,
+	   replace/4
 	  ]).
 
 :- use_module(library(ref_replace)).
@@ -15,27 +16,32 @@
     replace_sentence(+,-,0,+),
     replace_goal(+,-,0,+).
 
-%%  replace_goal(?Goal, ?Into, :Expander, +Action)
+%%  replace_goal(?Goal, ?Into, :Expander, +Options)
 %
 replace_goal(Goal, Into, Expander, Options) :-
     replace(goal, Goal, Into, Expander, Options).
 
-%%  replace_term(?Term, ?Into, :Expander, +Action)
+%%  replace_term(?Term, ?Into, :Expander, +Options)
 %
 replace_term(Term, Into, Expander, Options) :-
     replace(term, Term, Into, Expander, Options).
 
-%%  replace_head(?Term, ?Into, :Expander, +Action)
+%%  replace_head(?Term, ?Into, :Expander, +Options)
 %
 replace_head(Term, Into, Expander, Options) :-
     replace(head, Term, Into, Expander, Options).
 
-%%  replace_body(?Term, ?Into, :Expander, +Action)
+%%  replace_body(?Term, ?Into, :Expander, +Options)
 %
 replace_body(Term, Into, Expander, Options) :-
     replace(body, Term, Into, Expander, Options).
 
-%%  replace_sentence(?Sentence, ?Into, :Expander, +Action).
+%%  replace_sentence(?Sentence, ?Into, :Expander, +Options).
 %
 replace_sentence(Sentence, Into, Expander, Options) :-
     replace(sent, Sentence, Into, Expander, Options).
+
+%%  replace(+Scope, ?Term, ?Into, :Expander, +Options).
+%
+replace(Scope, Term, Into, Options) :-
+    replace(Scope, Term, Into, true, Options).
