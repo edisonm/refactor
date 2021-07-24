a1c3dc9b03df34451474978b203434a4506ff852
Edison Mera <efmera@gmail.com>
2021-07-24 19:55:50 +0200

    Edison Mera Menendez ---> Edison Mera

--- pack.pl
+++ pack.pl
@@ -2,9 +2,9 @@ name(refactor).
 version('0.0.3').
 title('Refactoring Tools for SWI-Prolog').
 keywords([refactor]).
-author('Edison Mera Menendez', 'http://www.edisonm.com/').
-packager('Edison Mera Menendez', 'http://www.edisonm.com/').
-maintainer('Edison Mera Menendez', 'http://www.edisonm.com/').
+author('Edison Mera', 'http://www.edisonm.com/').
+packager('Edison Mera', 'http://www.edisonm.com/').
+maintainer('Edison Mera', 'http://www.edisonm.com/').
 home('https://github.com/edisonm/refactor').
 download('https://github.com/edisonm/refactor.git').
 requires(xtools).
