67eaf81718e82fee5937429beb3e7c7d931575c3
Edison Mera <efmera@gmail.com>
2023-12-02 15:58:03 +0100

    Updated documentation of ref_replay.pl

--- prolog/ref_replay.pl
+++ prolog/ref_replay.pl
@@ -52,14 +52,20 @@
 :- use_module(library(ref_msgtype)).
 :- use_module(library(ref_command)).
 
-/** <module> Pending changes management of individual changes
+/** <module> Individual pending changes management
 
-  This library provides tools to manage individual elements of the stack of
+  This library  provides tools  to manage  individual elements  of the  stack of
   pending changes of the refactoring tool.  The pending changes are indexed by a
   sequential number, so that you can use such index in such predicates.
+
 */
 
 :- meta_predicate apply_command_q(0).
+
+%!  apply_command_q(:Call).
+
+%   Execute Call and  associated such call with an index  for further management
+%   of such Call.
 
 apply_command_q(Call) :-
     apply_command(Call),
@@ -67,25 +73,55 @@ apply_command_q(Call) :-
     message_type(Type),
     print_message(Type, format('Saved changes in index ~w', [Index])).
 
+%!  rstats is det.
+%
+%   Stablish that it must  be printed an information message at at  the end of a
+%   command.
+
 rstats :-
     retractall(rstats_db),
     assertz(rstats_db).
 
+%!  rnostats is det.
+%
+%   Stablish that it must be printed an informational message at at the end of a
+%   command, this means that only when the verbosity is the highest you will see
+%   that message.
+
 rnostats :-
     retractall(rstats_db).
+
+%!  rlist is det.
+%
+%   Display all commands that are waiting to be applied.
 
 rlist :-
     \+ ( rlist(_),
          fail
        ).
 
+%!  rlist(Index) is det.
+%
+%   Display on  backtracking the commands  that are  waiting to be  applied, and
+%   unifies Index with such command's index.
+
 rlist(Index) :-
     pending_command(Index, Command),
     with_output_to(string(SCommand), portray_clause(Command)),
     print_message(information, format('Index ~w, Command: ~s', [Index, SCommand])).
 
+%!  rrewind is det.
+%
+%   Reaply all the pending commands.  This is useful if you change a file before
+%   to commit the  changes, making the refactoring outdated with  respect to the
+%   current files.
+
 rrewind :-
     rrewind(0).
+
+%!  rrewind(Index) is det.
+%
+%   Reaply all the commands whose index is greater than Index.
 
 rrewind(Index) :-
     findall(Command, ( pending_command(Index1, Command),
@@ -96,16 +132,34 @@ rrewind(Index) :-
     reverse(CommandR, CommandL),
     maplist(apply_command, CommandL).
 
+%!  rundo is multi.
+%
+%   Undo refactoring commands on backtracking.
+
 rundo :-
     rundo(_).
+
+%!  rdelete(Index).
+%
+%   Undo and rewind refactoring commands on backtracking.
 
 rdelete(Index) :-
     rundo(Index),
     rrewind(Index).
 
+%!  rundo(-Index) is multi.
+%
+%   Undo refactoring commands on backtracking and unifies Index with the index
+%   of the removed command.
+
 rundo(Index) :-
     rdrop(Index, Command),
     print_message(information, format('Undone ~w ---> ~w', [Index, Command])).
+
+%!  rdrop(-Index) is multi.
+%
+%   Undo refactoring  commands on backtracking,  unifies Index and  Command with
+%   the index and the removed command respectively.
 
 rdrop(Index, Command) :-
     undo_changes(Index),
